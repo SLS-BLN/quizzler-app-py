22b3b40d5cbbb4d18df7afc6c1b441b30b98d13f
Stefan Schlicker <stefanschlicker@posteo.de>
2025-08-24 09:00:45 +0200

    add logic to buttons, give feedback to user

--- ui.py
+++ ui.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.root = tk.Tk()
         self.root.title("Quizzler")
-        self.root.config(bg=THEME_COLOR)
+        self.root.config(padx=20, pady=20, bg=THEME_COLOR)
 
         # Load images
         self.true_img = tk.PhotoImage(file="images/true.png")
@@ -14,9 +17,10 @@ class QuizInterface:
 
         # Create label and canvas
         self.score_label = tk.Label(self.root, text="Score", fg="white", bg=THEME_COLOR, font=("Arial", 12, "bold"))
-        self.canvas = tk.Canvas(self.root, width=400, height=400, bg="white")
-        self.canvas.create_text(
-            200, 200,  # X and Y coordinates (center of canvas)
+        self.canvas = tk.Canvas(self.root, width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 125,  # X and Y coordinates (center of canvas)
+            width=280,
             text="Your quiz starts now!",  # Replace with your desired text
             font=("Arial", 20, "italic"),
             fill=THEME_COLOR  # Or any color you like
@@ -27,15 +31,38 @@ class QuizInterface:
         self.false_button = tk.Button(self.root, image=self.false_img, highlightthickness=0, bd=0, command=self.false_pressed)
 
         # Place items in a 2-column grid
-        self.score_label.grid(row=0, column=1, padx=10, pady=10)
-        self.canvas.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-        self.true_button.grid(row=2, column=0, padx=10, pady=10)
-        self.false_button.grid(row=2, column=1, padx=10, pady=10)
+        self.score_label.grid(row=0, column=1)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=40)
+        self.true_button.grid(row=2, column=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
 
         self.root.mainloop()
 
     def true_pressed(self):
-        print("True button pressed")
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
 
     def false_pressed(self):
-        print("False button pressed")
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="#29B677")
+        else:
+            self.canvas.config(bg="#EE665D")
+
+        self.root.after(1000, self.get_next_question)
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfigure(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfigure(self.question_text, text=f"You've completed the quiz. Your final score is: {self.quiz.score}/{len(self.quiz.question_list)}.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
